1a5f1292961f18edf29f46c6799e1e4d05f1d469
ptan044 <tanpinda@gmail.com>
2025-02-27 15:25:52 +0000

    [34] WIP create feature tracker node

--- feature_tracker_ros/src/feature_tracker_node.cpp
+++ feature_tracker_ros/src/feature_tracker_node.cpp
@@ -1,234 +1,100 @@
-#include <ros/ros.h>
-#include <sensor_msgs/Image.h>
-#include <sensor_msgs/image_encodings.h>
-#include <sensor_msgs/PointCloud.h>
-#include <sensor_msgs/Imu.h>
-#include <std_msgs/Bool.h>
+#include "feature_tracker_ros/feature_tracker_node.hpp"
+
 #include <cv_bridge/cv_bridge.h>
-#include <message_filters/subscriber.h>
-#include "feature_tracker/feature_tracker.h"
-#include "ros_parameter_reader.h"
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud.h>
+#include <opencv2/opencv.hpp>
+#include <iomanip>
 
-#define SHOW_UNDISTORTION 0
+bool FeatureTrackerNode::Start() {
+  restart_flag_.data = true;
 
-vector<uchar> r_status;
-vector<float> r_err;
-queue<sensor_msgs::ImageConstPtr> img_buf;
+  feature_points_msg_.channels.push_back(feature_id_channel_);
+  feature_points_msg_.channels.push_back(feature_u_pixel_channel_);
+  feature_points_msg_.channels.push_back(feature_v_pixel_channel_);
+  feature_points_msg_.channels.push_back(feature_x_velocity_channel_);
+  feature_points_msg_.channels.push_back(feature_y_velocity_channel_);
 
-ros::Publisher pub_img, pub_match;
-ros::Publisher pub_restart;
+  feature_id_channel_.name = "Feature Ids";
+  feature_u_pixel_channel_.name = "Feature U Pixel";
+  feature_v_pixel_channel_.name = "Feature V Pixel";
+  feature_x_velocity_channel_.name = "Feature X Velocity";
+  feature_y_velocity_channel_.name = "Feature Y Velocity";
 
-FeatureTracker trackerData(); 
-double first_image_time;
-int pub_count = 1;
-bool first_image_flag = true;
-double last_image_time = 0;
-bool init_pub = 0;
-
-
-
-
-void img_callback(const sensor_msgs::ImageConstPtr &img_msg) {
-  trackerData.ProcessNewFrame()
-  if (first_image_flag) {
-    first_image_flag = false;
-    first_image_time = img_msg->header.stamp.toSec();
-    last_image_time = img_msg->header.stamp.toSec();
-    return;
-  }
-  // detect unstable camera stream
-  if (img_msg->header.stamp.toSec() - last_image_time > 1.0 ||
-      img_msg->header.stamp.toSec() < last_image_time) {
-    ROS_WARN("image discontinue! reset the feature tracker!");
-    first_image_flag = true;
-    last_image_time = 0;
-    pub_count = 1;
-    std_msgs::Bool restart_flag;
-    restart_flag.data = true;
-    pub_restart.publish(restart_flag);
-    return;
-  }
-  last_image_time = img_msg->header.stamp.toSec();
-  // frequency control
-  if (round(1.0 * pub_count /
-            (img_msg->header.stamp.toSec() - first_image_time)) <= FREQ) {
-    PUB_THIS_FRAME = true;
-    // reset the frequency control
-    if (abs(1.0 * pub_count /
-                (img_msg->header.stamp.toSec() - first_image_time) -
-            FREQ) < 0.01 * FREQ) {
-      first_image_time = img_msg->header.stamp.toSec();
-      pub_count = 0;
-    }
-  } else
-    PUB_THIS_FRAME = false;
-
-  cv_bridge::CvImageConstPtr ptr;
-  if (img_msg->encoding == "8UC1") {
-    sensor_msgs::Image img;
-    img.header = img_msg->header;
-    img.height = img_msg->height;
-    img.width = img_msg->width;
-    img.is_bigendian = img_msg->is_bigendian;
-    img.step = img_msg->step;
-    img.data = img_msg->data;
-    img.encoding = "mono8";
-    ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::MONO8);
-  } else
-    ptr = cv_bridge::toCvCopy(img_msg, sensor_msgs::image_encodings::MONO8);
-
-  cv::Mat show_img = ptr->image;
-  TicToc t_r;
-  for (int i = 0; i < NUM_OF_CAM; i++) {
-    ROS_DEBUG("processing camera %d", i);
-    if (i != 1 || !STEREO_TRACK)
-      trackerData[i].readImage(ptr->image.rowRange(ROW * i, ROW * (i + 1)),
-                               img_msg->header.stamp.toSec());
-    else {
-      if (EQUALIZE) {
-        cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE();
-        clahe->apply(ptr->image.rowRange(ROW * i, ROW * (i + 1)),
-                     trackerData[i].cur_img);
-      } else
-        trackerData[i].cur_img = ptr->image.rowRange(ROW * i, ROW * (i + 1));
-    }
-
-#if SHOW_UNDISTORTION
-    trackerData[i].showUndistortion("undistrotion_" + std::to_string(i));
-#endif
-  }
-
-  for (unsigned int i = 0;; i++) {
-    bool completed = false;
-    for (int j = 0; j < NUM_OF_CAM; j++)
-      if (j != 1 || !STEREO_TRACK) completed |= trackerData[j].updateID(i);
-    if (!completed) break;
-  }
-
-  if (PUB_THIS_FRAME) {
-    pub_count++;
-    sensor_msgs::PointCloudPtr feature_points(new sensor_msgs::PointCloud);
-    sensor_msgs::ChannelFloat32 id_of_point;
-    sensor_msgs::ChannelFloat32 u_of_point;
-    sensor_msgs::ChannelFloat32 v_of_point;
-    sensor_msgs::ChannelFloat32 velocity_x_of_point;
-    sensor_msgs::ChannelFloat32 velocity_y_of_point;
-
-    feature_points->header = img_msg->header;
-    feature_points->header.frame_id = "world";
-
-    vector<set<int>> hash_ids(NUM_OF_CAM);
-    for (int i = 0; i < NUM_OF_CAM; i++) {
-      auto &un_pts = trackerData[i].cur_un_pts;
-      auto &prev_pts = trackerData[i].prev_pts;
-      auto &ids = trackerData[i].ids;
-      auto &pts_velocity = trackerData[i].pts_velocity;
-      for (unsigned int j = 0; j < ids.size(); j++) {
-        if (trackerData[i].track_cnt[j] > 1) {
-          int p_id = ids[j];
-          hash_ids[i].insert(p_id);
-          geometry_msgs::Point32 p;
-          p.x = un_pts[j].x;
-          p.y = un_pts[j].y;
-          p.z = 1;
-
-          feature_points->points.push_back(p);
-          id_of_point.values.push_back(p_id * NUM_OF_CAM + i);
-          u_of_point.values.push_back(prev_pts[j].x);
-          v_of_point.values.push_back(prev_pts[j].y);
-          velocity_x_of_point.values.push_back(pts_velocity[j].x);
-          velocity_y_of_point.values.push_back(pts_velocity[j].y);
-        }
-      }
-    }
-    feature_points->channels.push_back(id_of_point);
-    feature_points->channels.push_back(u_of_point);
-    feature_points->channels.push_back(v_of_point);
-    feature_points->channels.push_back(velocity_x_of_point);
-    feature_points->channels.push_back(velocity_y_of_point);
-    ROS_DEBUG("publish %f, at %f", feature_points->header.stamp.toSec(),
-              ros::Time::now().toSec());
-    // skip the first image; since no optical speed on frist image
-    if (!init_pub) {
-      init_pub = 1;
-    } else
-      pub_img.publish(feature_points);
-
-    if (SHOW_TRACK) {
-      ptr = cv_bridge::cvtColor(ptr, sensor_msgs::image_encodings::BGR8);
-      // cv::Mat stereo_img(ROW * NUM_OF_CAM, COL, CV_8UC3);
-      cv::Mat stereo_img = ptr->image;
-
-      for (int i = 0; i < NUM_OF_CAM; i++) {
-        cv::Mat tmp_img = stereo_img.rowRange(i * ROW, (i + 1) * ROW);
-        cv::cvtColor(show_img, tmp_img, CV_GRAY2RGB);
-
-        for (unsigned int j = 0; j < trackerData[i].prev_pts.size(); j++) {
-          double len =
-              std::min(1.0, 1.0 * trackerData[i].track_cnt[j] / WINDOW_SIZE);
-          cv::circle(tmp_img, trackerData[i].prev_pts[j], 2,
-                     cv::Scalar(255 * (1 - len), 0, 255 * len), 2);
-          // draw speed line
-          /*
-          Vector2d tmp_cur_un_pts (trackerData[i].cur_un_pts[j].x,
-          trackerData[i].cur_un_pts[j].y); Vector2d tmp_pts_velocity
-          (trackerData[i].pts_velocity[j].x, trackerData[i].pts_velocity[j].y);
-          Vector3d tmp_prev_un_pts;
-          tmp_prev_un_pts.head(2) = tmp_cur_un_pts - 0.10 * tmp_pts_velocity;
-          tmp_prev_un_pts.z() = 1;
-          Vector2d tmp_prev_uv;
-          trackerData[i].m_camera->spaceToPlane(tmp_prev_un_pts, tmp_prev_uv);
-          cv::line(tmp_img, trackerData[i].prev_pts[j],
-          cv::Point2f(tmp_prev_uv.x(), tmp_prev_uv.y()), cv::Scalar(255 , 0, 0),
-          1 , 8, 0);
-          */
-          // char name[10];
-          // sprintf(name, "%d", trackerData[i].ids[j]);
-          // cv::putText(tmp_img, name, trackerData[i].prev_pts[j],
-          // cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
-        }
-      }
-      // cv::imshow("vis", stereo_img);
-      // cv::waitKey(5);
-      pub_match.publish(ptr->toImageMsg());
-    }
-  }
-  ROS_INFO("whole feature tracker processing costs: %f", t_r.toc());
+  // Set this upon first camera message received
+  optical_flow_img_.header.frame_id = "TODO";
+  optical_flow_img_.encoding = sensor_msgs::image_encodings::BGR8;
+  return true;
 }
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "feature_tracker");
-  ros::NodeHandle n("~");
-  ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME,
-                                 ros::console::levels::Info);
-  readParameters(n);
-
-  for (int i = 0; i < NUM_OF_CAM; i++)
-    trackerData[i].readIntrinsicParameter(CAM_NAMES[i]);
-
-  if (FISHEYE) {
-    for (int i = 0; i < NUM_OF_CAM; i++) {
-      trackerData[i].fisheye_mask = cv::imread(FISHEYE_MASK, 0);
-      if (!trackerData[i].fisheye_mask.data) {
-        ROS_INFO("load mask fail");
-        ROS_BREAK();
-      } else
-        ROS_INFO("load mask success");
-    }
-  }
-
-  ros::Subscriber sub_img = n.subscribe(IMAGE_TOPIC, 100, img_callback);
-
-  pub_img = n.advertise<sensor_msgs::PointCloud>("feature", 1000);
-  pub_match = n.advertise<sensor_msgs::Image>("feature_img", 1000);
-  pub_restart = n.advertise<std_msgs::Bool>("restart", 1000);
-  /*
-  if (SHOW_TRACK)
-      cv::namedWindow("vis", cv::WINDOW_NORMAL);
-  */
-  ros::spin();
-  return 0;
+void FeatureTrackerNode::OnRegistered() {
+  ROS_INFO("FeatureTrackerNode has been registered by feature tracker");
 }
 
-// new points velocity is 0, pub or not?
-// track cnt > 1 pub?
+void FeatureTrackerNode::OnRestart() {
+  ROS_INFO("FeatureTracker has been restarted");
+  pub_restart_.publish(restart_flag_);
+}
+
+void FeatureTrackerNode::OnDurationBetweenFrameTooLarge(
+    double current_image_time_s, double previous_image_time_s) {
+  ROS_INFO_STREAM("Too much time has elapsed between images."
+                  << "\n\t Current image received had timestamp: "
+                  << std::setprecision(2) << current_image_time_s
+                  << "\n\t Previous image received had timestamp:  "
+                  << std::setprecision(2) << previous_image_time_s);
+}
+
+void FeatureTrackerNode::OnImageTimeMovingBackwards(
+    double current_image_time_s, double previous_image_time_s) {
+  ROS_WARN_STREAM("Timestamp went backwards/"
+                  << "\n\t Current image received had timestamp: "
+                  << std::setprecision(2) << current_image_time_s
+                  << "\n\t Previous image received had timestamp:  "
+                  << std::setprecision(2) << previous_image_time_s);
+}
+void FeatureTrackerNode::OnProcessedImage(
+    cv::Mat new_frame, double current_image_time_s,
+    std::vector<cv::Point2f> features,
+    std::vector<cv::Point2f> undistorted_features, std::vector<int> ids,
+    std::vector<int> track_count, std::vector<cv::Point2f> points_velocity) {
+  feature_points_msg_.points.clear();
+  feature_id_channel_.values.clear();
+  feature_u_pixel_channel_.values.clear();
+  feature_v_pixel_channel_.values.clear();
+  feature_x_velocity_channel_.values.clear();
+  feature_y_velocity_channel_.values.clear();
+
+  feature_points_msg_.header.stamp = current_image_time_;
+  for (size_t i = 0; i < ids.size(); i++) {
+    if (track_count[i] > min_track_count_to_publish_) {
+      geometry_msgs::Point32 p;
+      p.x = undistorted_features[i].x;
+      p.y = undistorted_features[i].y;
+      p.z = 1;
+
+      feature_points_msg_.points.push_back(p);
+      feature_id_channel_.values.push_back(ids[i]);
+      feature_u_pixel_channel_.values.push_back(features[i].x);
+      feature_v_pixel_channel_.values.push_back(features[i].y);
+      feature_x_velocity_channel_.values.push_back(points_velocity[i].x);
+      feature_y_velocity_channel_.values.push_back(points_velocity[i].y);
+    }
+  }
+  optical_flow_img_.header.stamp = current_image_time_;
+  optical_flow_img_.image = CreateOpticalFlowImage(
+      new_frame, features, track_count, 20, points_velocity);
+  optical_flow_img_.toImageMsg(optical_flow_img_msg_);
+  optical_flow_image_publisher_.publish(optical_flow_img_msg_);
+  feature_point_cloud_publisher_.publish(feature_points_msg_);
+}
+
+void FeatureTrackerNode::img_callback(
+    const sensor_msgs::ImageConstPtr& img_msg) {
+  current_image_time_ = img_msg->header.stamp;
+  cv_bridge::CvImagePtr cv_ptr =
+      cv_bridge::toCvShare(img_msg, sensor_msgs::image_encodings::BGR8);
+//   feature_tracker.ProcessNewFrame(cv_ptr->image);
+}
+
+int main(int argc, char** argv) {}
